62c91f6de32ff843d87d1d75c1fce185e7b56157
hirotoshi217 <littledaddy375@gmail.com>
2025-01-25 17:27:38 -0800

    Update user_models.py

--- models/user_models.py
+++ models/user_models.py
@@ -7,9 +7,9 @@ class Users(UserMixin, db.Model):
     __tablename__ = 'users'
 
     user_id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(50), unique=True, nullable=False)
-    user_password = db.Column(db.String(150), nullable=False)
-    role = db.Column(db.String(10), nullable=False, default="user")
+    user_name = db.Column(db.String(100), unique=True, nullable=False)
+    user_password = db.Column(db.String(5000), nullable=False)
+    role = db.Column(db.String(100), nullable=False, default="user")
 
     # 単一の期にだけアクセスできる場合に使用 (adminはNULLで全期OKなど)
     generation_id = db.Column(db.Integer, nullable=True)
