a4169613bdc26e66b667f2a16dde1bc4474e4718
hirotoshi217 <littledaddy375@gmail.com>
2025-02-28 17:32:55 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,10 @@
 # config.py
 # 必要に応じて Flask の設定クラスを作り分けるサンプル
-
 import os
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'SUPER_SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
