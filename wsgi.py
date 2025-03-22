9a93c4df749c0e755f66885df1a4c4dcef0a63f8
hirotoshi217 <littledaddy375@gmail.com>
2025-03-22 08:25:40 -0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,9 @@
-from app import create_app
+from app import create_app, db
+from flask_migrate import Migrate
+import models  # models 内の各ファイル（trade_models.py など）が読み込まれるように
 
 app = create_app()
+migrate = Migrate(app, db)
+
+if __name__ == '__main__':
+    app.run()
