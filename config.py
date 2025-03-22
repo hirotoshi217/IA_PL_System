fe7fa2783b5220968a2be71c34f650a00a1587ad
hirotoshi217 <littledaddy375@gmail.com>
2025-03-22 10:41:25 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'SUPER_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
