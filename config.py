b3b828488d2f64c4b34784748e587221effa1a9b
hirotoshi217 <littledaddy375@gmail.com>
2025-03-25 14:50:00 -0700

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 
 class BaseConfig:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'SUPER_SECRET_KEY')
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3') # migration用
+    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3') migration用
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
